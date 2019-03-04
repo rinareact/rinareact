6da17822537c46b7d65da0962bc7981f1eee13f0
chengjun <439860966@qq.com>
2019-03-04 17:16:51 +0800

    增加了ant的轮播图

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,26 @@
 import React, { Component} from 'react'
 import './home.scss'
+import { Carousel, WingBlank } from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css';
 
 class Com extends Component{
+    constructor (props) {
+        super(props);
+        this.state = {
+            data: ['1', '2', '3'],
+            imgHeight: 200,
+        }
+    }
+
+    componentDidMount() {
+    // simulate img loading
+        setTimeout(() => {
+            this.setState({
+            data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
+            });
+        }, 100);
+    }
+
     render() {
         return(
             <div className="box">
@@ -13,8 +32,54 @@ class Com extends Component{
                     <div className="header-r">搜索</div>
                 </header>
                 <div className="banner">
-                    轮播图
+                    <WingBlank>
+                        <Carousel
+                        autoplay={false}
+                        infinite
+                        beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+                        afterChange={index => console.log('slide to', index)}
+                        >
+                        {this.state.data.map(val => (
+                            <a
+                            key={val}
+                            href="http://www.alipay.com"
+                            style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                            >
+                            <img
+                                src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                                alt=""
+                                style={{ width: '100%', verticalAlign: 'top' }}
+                                onLoad={() => {
+                                // fire window resize event to change height
+                                window.dispatchEvent(new Event('resize'));
+                                this.setState({ imgHeight: '200' });
+                                }}
+                            />
+                            </a>
+                        ))}
+                        </Carousel>
+                    </WingBlank>
                 </div>
+                <nav className="navlist">
+                    <ul>
+                        <li>
+                            <img src='/images/t1.png' alt=""/>
+                            <h4>热门</h4>
+                        </li>
+                        <li>
+                            <img src="/images/t2.png" alt=""/>
+                            <h4>精品</h4>
+                        </li>
+                        <li>
+                            <img src="/images/t3.png" alt=""/>
+                            <h4>专题列表</h4>
+                        </li>
+                        <li>
+                            <img src="/images/t4.png" alt=""/>
+                            <h4>品牌</h4>
+                        </li>
+                    </ul>
+                </nav>
                 <div className="content">
                     首页内容
                 </div>
