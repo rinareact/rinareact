b01ff554fd24aeb258c4a019bd242d74f722ef87
chengjun <439860966@qq.com>
2019-03-04 22:20:30 +0800

    从主页分离了List组件

---
+++ src/components/home/List.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import './List.scss';
+
+const List = () => (
+  <div className="shoplist">
+      <div className="tb_box">
+          <h2 className="tb_tit">
+              礼品箱包
+              <a className='more'>更多&gt;</a>
+          </h2>
+          <div className="tb_type">
+              <a className="tb_floor">
+                  <img src="/images/t5.jpg" alt=""/>
+              </a>
+              <a className="tb_link tb_special">
+                  <img src="/images/t6.jpg" alt=""/>
+              </a>
+              <a className="tb_link">
+                  <img src="/images/t7.jpg" alt=""/>
+              </a>
+          </div>
+      </div>
+      <div className="tb_box">
+          <h2 className="tb_tit">
+              礼品箱包
+              <a className='more'>更多&gt;</a>
+          </h2>
+          <div className="tb_type">
+              <a className="tb_floor">
+                  <img src="/images/t5.jpg" alt=""/>
+              </a>
+              <a className="tb_link tb_special">
+                  <img src="/images/t6.jpg" alt=""/>
+              </a>
+              <a className="tb_link">
+                  <img src="/images/t7.jpg" alt=""/>
+              </a>
+          </div>
+      </div>
+      <div className="tb_box">
+          <h2 className="tb_tit">
+              礼品箱包
+              <a className='more'>更多&gt;</a>
+          </h2>
+          <div className="tb_type">
+              <a className="tb_floor">
+                  <img src="/images/t5.jpg" alt=""/>
+              </a>
+              <a className="tb_link tb_special">
+                  <img src="/images/t6.jpg" alt=""/>
+              </a>
+              <a className="tb_link">
+                  <img src="/images/t7.jpg" alt=""/>
+              </a>
+          </div>
+      </div>
+    </div>
+)
+
+export default List
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React, { Component} from 'react'
 import './home.scss'
 import { Carousel, WingBlank } from 'antd-mobile';
 import 'antd-mobile/dist/antd-mobile.css';
+import List from '@/components/home/List.jsx';
 
 class Com extends Component{
     constructor (props) {
@@ -80,59 +81,7 @@ class Com extends Component{
                         </li>
                     </ul>
                 </nav>
-                <div className="shoplist">
-                    <div className="tb_box">
-                        <h2 className="tb_tit">
-                            礼品箱包
-                            <a className='more'>更多&gt;</a>
-                        </h2>
-                        <div className="tb_type">
-                            <a className="tb_floor">
-                                <img src="/images/t5.jpg" alt=""/>
-                            </a>
-                            <a className="tb_link tb_special">
-                                <img src="/images/t6.jpg" alt=""/>
-                            </a>
-                            <a className="tb_link">
-                                <img src="/images/t7.jpg" alt=""/>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="tb_box">
-                        <h2 className="tb_tit">
-                            礼品箱包
-                            <a className='more'>更多&gt;</a>
-                        </h2>
-                        <div className="tb_type">
-                            <a className="tb_floor">
-                                <img src="/images/t5.jpg" alt=""/>
-                            </a>
-                            <a className="tb_link tb_special">
-                                <img src="/images/t6.jpg" alt=""/>
-                            </a>
-                            <a className="tb_link">
-                                <img src="/images/t7.jpg" alt=""/>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="tb_box">
-                        <h2 className="tb_tit">
-                            礼品箱包
-                            <a className='more'>更多&gt;</a>
-                        </h2>
-                        <div className="tb_type">
-                            <a className="tb_floor">
-                                <img src="/images/t5.jpg" alt=""/>
-                            </a>
-                            <a className="tb_link tb_special">
-                                <img src="/images/t6.jpg" alt=""/>
-                            </a>
-                            <a className="tb_link">
-                                <img src="/images/t7.jpg" alt=""/>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                <List />
             </div>
         )
     }
