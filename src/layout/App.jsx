45185e14d77cc1e51476b6b67b5afd7083beb20e
Teemowei <1061779661@qq.com>
2019-03-05 22:59:07 +0800

    登入静态

--- src/layout/App.jsx
+++ src/layout/App.jsx
@@ -4,6 +4,8 @@ import Home from '../pages/Home';
 import Kind from '../pages/Kind';
 import Cart from '../pages/Cart';
 import User from '../pages/User';
+import Login from '../pages/Login';
+import Res from '../pages/Res';
 import Nav from "../components/Nav/Nav";
 
 class  App extends Component{
@@ -16,6 +18,8 @@ class  App extends Component{
                     <Route path="/kind" component = {Kind}/>
                     <Route path="/cart" component = {Cart}/>
                     <Route path="/user" component = {User}/>
+                    <Route path="/login" component = {Login}/>
+                    <Route path="/res" component = {Res}/>
                     <Redirect path="/" to="/home"/>
                 </Switch>
                 <Nav />
