b3ffb3dd691021ddb53ad07adc92587530191e66
RinaXue <2965675890@qq.com>
2019-03-08 19:16:04 +0800

    解决冲突

--- src/index.js
+++ src/index.js
@@ -6,33 +6,21 @@ import {Provider} from 'react-redux';
 
 
 import App from './layout/App';
-<<<<<<< HEAD
-import CartApp from './layout/CartApp';
-=======
+
 import UserApp from './layout/UserApp';
 
->>>>>>> dev
 import 'antd-mobile/dist/antd-mobile.css';
 import './main.scss';
 
 
 ReactDOM.render(
-<<<<<<< HEAD
     <Provider store={store}>
         <BrowserRouter>
             <Switch>
-                <Route path='/cart' component ={CartApp}/>
+                <Route path='/userapp' component ={UserApp}/>
                 <Route path='/' component ={App}/>
             </Switch>
         </BrowserRouter>
     </Provider>,
-=======
-    <BrowserRouter>
-        <Switch>
-            <Route path='/userapp' component ={UserApp}/>
-            <Route path='/' component ={App}/>
-        </Switch>
-    </BrowserRouter>,
->>>>>>> dev
     document.getElementById('root')
 );
