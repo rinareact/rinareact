66d7d784262996cf6b7d32ad24084e589abf3770
Teemowei <1061779661@qq.com>
2019-03-11 22:46:06 +0800

    Revert "合并了大举的分类，请求到了数据"
    This reverts commit ed22643748fcdc53b7f7679cfbe2f9cd0347ff61.

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,8 @@
-// let baseUrl =  'api'
-// process.env.NODE_ENV === 'development' ? baseUrl = 'api' : baseUrl = 'http://39.96.196.70:3000/api/'
+let baseUrl =  'api'
+process.env.NODE_ENV === 'development' ? baseUrl = 'api' : baseUrl = 'http://39.96.196.70:3000/api/'
 
-// 分类接口
-let baseUrl= '/api'
-process.env.NODE_ENV === 'development' ? baseUrl = '/api' : baseUrl = 'http://39.96.196.70:3000'
-
-//登入注册接口
 let baseUrlcw = 'https://www.daxunxun.com'
 // process.env.NODE_ENV === 'development' ? baseUrl = '/daxun' : baseUrl = 'https://www.daxunxun.com'
-process.env.NODE_ENV === 'development' ? baseUrlcw = '/daxun' : baseUrlcw = 'https://www.daxunxun.com'
+process.env.NODE_ENV === 'development' ? baseUrl = '/daxun' : baseUrl = 'https://www.daxunxun.com'
 
 export default {baseUrl,baseUrlcw}
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -12,14 +12,4 @@ module.exports = function(app) {
       }
     )
   );
-  app.use(
-    proxy(
-      "/api", {
-      target: "http://39.96.196.70:3000",
-      changeOrigin: true,
-      pathRewrite: { // 自己配置代理必须加这句话
-        '^/api': ''
-      }
-    })
-  );
 };
